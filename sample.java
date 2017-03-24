19124933ee24c183d3531fbeb1d1a1e1487a835a
vishnu <gvishnu415@gmail.com>
2017-03-23 23:55:55 -0500

    changed

--- sample.java
+++ sample.java
@@ -1,6 +1,6 @@
 class HelloWorld {
    public static void main(String[] args) {
     Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, World");
+      System.out.println("Hello2World");
    }
 }
